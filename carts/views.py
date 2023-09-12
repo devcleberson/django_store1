ab55726cbc63b2838756c132de60d38088623896
devcleberson <devcleberson@gmail.com>
2023-09-11 21:48:31 -0300

    implement_data_into_cart_page

--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from store.models import Product
 from .models import Cart, CartItem
-from django.core.exceptions import ObjectDoesNotExist
-
 
 from django.http import HttpResponse
-
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -34,24 +31,21 @@ def add_cart(request, product_id):
             cart = cart,
         )
         cart_item.save()
-    return HttpResponse(cart_item.quantity)  
-    exit()
     return redirect('cart')    
-        
-    
+         
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filters(cart=cart, is_active=True)
-        for cart_item in cart_item:
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-    except ObjectDoesNotExist:
+    except ObjectNotExist:
         pass #just ignore
     
     context = {
         'total': total,
         'quantity': quantity,
-        'cart_items': cart_item,
+        'cart_items': cart_items,
     }      
     return render(request, 'store/cart.html', context)
