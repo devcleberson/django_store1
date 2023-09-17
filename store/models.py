7986993b4743a66833b087ebccde3f7877d72f22
devcleberson <devcleberson@gmail.com>
2023-09-16 21:20:10 -0300

    product_variation_model

--- store/models.py
+++ store/models.py
@@ -2,6 +2,8 @@ from django.urls import reverse
 from category.models import Category
 from django.db import models
 
+
+
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -19,3 +21,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+    
+variation_category_choice = {
+    ('color', 'color'),
+    ('size', 'size'),
+}
+
+class Variation(models.Model):
+       product            = models.ForeignKey(Product, on_delete=models.CASCADE)
+       variation_category = models.CharField(max_length=100, choices=variation_category_choice) 
+       variation_value    = models.CharField(max_length=100)
+       is_active          = models.BooleanField(default=True)
+       created_date       = models.DateTimeField(auto_now=True)
+       
+       def __unicode__(self):
+           return self.product
