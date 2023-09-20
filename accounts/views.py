377a7bea15dc9cab21359f090673ea5a0805818b
devcleberson <devcleberson@gmail.com>
2023-09-19 21:36:39 -0300

    registration_preparation_setting_up_urls_and_design

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .forms import RegistrationForm
+
+def register(request):
+    form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return
