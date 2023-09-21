07f2fffc5ab7c2907fda11df395c19183036c23f
devcleberson <devcleberson@gmail.com>
2023-09-20 23:00:57 -0300

    registration_making_view_and_editing_model_form_clean_method_to_check_passwords

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,4 +22,14 @@ class RegistrationForm(forms.ModelForm):
         self.fields['phone_number'].widget.attrs['placeholder'] = 'Digite o número de telefone' 
         self.fields['email'].widget.attrs['placeholder'] = 'Digite o endereço de e-mail' 
         for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'form-control'    
+            self.fields[field].widget.attrs['class'] = 'form-control'
+            
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )           
