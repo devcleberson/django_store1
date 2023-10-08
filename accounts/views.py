7293aae09562707c47ceed5d29eaba861316c93c
devcleberson <devcleberson@gmail.com>
2023-10-08 01:10:19 -0300

    user_login_functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -25,7 +26,23 @@ def register(request):
     return render(request, 'accounts/register.html', context)
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        user = auth.authenticate(email=email, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+           # messages.success(request, 'Você está conectado agora.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Credenciais de login inválidas.')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
+@login_required(login_url = 'login')
 def logout(request):
-    return
+    auth.logout(request)
+    messages.success(request, 'Você está desconectado.')
+    return redirect('login')
