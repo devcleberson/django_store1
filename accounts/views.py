1ace300d1bd5ecedf2fa8ebe3ffb8e4681d9b456
devcleberson <devcleberson@gmail.com>
2024-03-03 19:41:27 -0300

    forgot-password

--- accounts/views.py
+++ accounts/views.py
@@ -94,4 +94,35 @@ def activate(request, uidb64, token):
 @login_required(login_url = 'login')   
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
+
+
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+            
+            # Reset password email
+            current_site = get_current_site(request)
+            mail_subject = 'Redefinir sua senha'
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user': user, 
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            
+            messages.success(request, 'O e-mail de redefinição de senha foi enviado para seu endereço de e-mail.')
+            return redirect('login') 
+        else:
+            messages.error(request, 'Conta não existe!')   
+            return redirect('forgotPassword') 
+    return render(request, 'accounts/forgotPassword.html')
+
+
+def resetpassword_validate(request):
+    return HttpResponse('OK')
     
--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -144,7 +144,7 @@ MESSAGE_TAGS = {
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # SMTP configuration
